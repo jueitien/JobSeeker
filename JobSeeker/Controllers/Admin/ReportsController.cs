b404f3cba4482c93e6430f573e5d66be97799e7a
jueitien <jueitien@gmail.com>
2026-08-06 15:39:01 +0800

    Reorganise admin files into Controllers/Admin and Views/Admin subfolders

--- JobSeeker/Controllers/Admin/ReportsController.cs
+++ JobSeeker/Controllers/Admin/ReportsController.cs
@@ -1,14 +1,14 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace JobSeeker.Controllers
+namespace JobSeeker.Controllers.Admin
 {
     [Authorize(Roles = "Administrator")]
     public class ReportsController : Controller
     {
         public IActionResult Index()
         {
-            return View();
+            return View("~/Views/Admin/Reports/Index.cshtml");
         }
     }
 }
